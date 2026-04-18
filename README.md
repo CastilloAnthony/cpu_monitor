67bbb1625a8c6a9c7d5082521798aefabcb19990
Anthony Castillo <anthony1093ca@gmail.com>
2026-04-18 12:28:08 -0700

    Now utilizes average cpu usage and displays recent CPU usage

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # afk_monitor
-A simple script written in python for windows computers to monitor the cpu usage of the device and switch the power plan according whether or not the device is idle or not. The default time till idle is 3 minute and the default idle scheme is Power saver while the default active scheme is High Performance. These settings can be changed by modifiying the settings.json file. The idle time is in seconds, and the schemes should be either "Power saver", "Balanced", or "High Performance"  
+A simple script written in python for windows 11 (and 10) computers to monitor the cpu usage of the device and switch the power plan according to whether or not the device is idle or not. The default time till idle is 3 minute and the default idle scheme is Power saver while the default active scheme is Balanced and the high CPU utilization scheme is High Performance. The plling speed, idle time delay, idle scheme, active shceme, and utilization threshold can all be changed in the settings.json file. The polling speed and idle time are in seconds, the threshold is as a percentage, and the schemes should be either "Power saver", "Balanced", or "High Performance".
 
-Run the start.py or the start.bat script to start the program.
+First run pip -r requirements.txt to install the required packages.
+Then run the start.py file or the start.bat script to start the program.
 
 ### Changelog
+- 04/18/26: Script now utilizes average CPU usage and idle time to determine which schemes to use. Displays the most recent CPU usage in the terminal.
 - 07/08/25: The script has transitioned to monitoring idleness as opposed to utilization.
 - 03/18/24: The script now recognizes if a single core is above 75% then it will switch to high performance. Otherwise it will take the averages of all cores to determine if it needs to be in power saver or balanced mode.
--- afk_monitor.py
+++ afk_monitor.py
@@ -1,35 +1,26 @@
+# Developed by Anthony Castillo
+# Last Updated: 04/18/2026
 import time
-import win32api
-# import win32con
-# from threading import Thread
-import subprocess
-# import logging
 import json
-# import os
+import subprocess
 from pathlib import Path
+from psutil import cpu_percent
+import win32api
 
 class Arbiter():
     def __init__(self) -> None:
-        # self.__threads = []
-        self.__running = False
+        self.__running, self.__afk = False, False
         self.__power_schemes = {}
-        self.__curr_scheme = self.get_curr_scheme()
-        # self.__idle_threshold = 10*1
-        self.__afk = False
+        self.__curr_scheme = self.get_curr_scheme_id()
+        self.__cpu_usage = cpu_percent(percpu=True)
+        self.__lastPoll = time.time()
     # end __init__
 
     def __del__(self) -> None:
-        # while len(self.__threads) > 0:
-        #     if self.__threads[-1].is_alive():
-        #         self.__threads[-1].join(1)
-        #         if self.__threads[-1].is_alive():
-        #             self.__threads[-1].terminate()
-        #     del self.__threads[-1]
-        # del self.__threads
-        del self.__running, self.__power_schemes, self.__afk, self.__curr_scheme#, self.__curr_scheme, self.__idle_threshold, self.__threads, self.__main_run, 
+        del self.__running, self.__afk, self.__power_schemes, self.__curr_scheme, self.__cpu_usage, self.__lastPoll
     # end __del__
 
-    def start(self) -> None:
+    def start(self) -> None: # Called externally
         self.__running = True
         self.read_settings()
         self.__power_schemes = self.get_schemes()
@@ -40,9 +31,24 @@ class Arbiter():
         return subprocess.run("powercfg -getactivescheme", capture_output=True).stdout.decode("ascii")
     # end get_curr_scheme
 
-    def get_schemes(self) -> list:
+    def get_curr_scheme_id(self) -> str:
+        raw_scheme = subprocess.run("powercfg -getactivescheme", capture_output=True).stdout.decode("ascii")
+        scheme, begin = '', False
+        for c in raw_scheme:
+            if begin and c != '\n' and c != '\r' and c != ' ':
+                scheme += c
+            elif not begin and c == ':' :
+                begin = True
+            elif c == ' ' and len(scheme) > 2:
+                scheme += c
+                break
+        return scheme
+    # end _getSchemeID
+
+    def get_schemes(self) -> dict:
         raw_list = subprocess.run("powercfg -l", capture_output=True)
         temp_list, temp_string, power_schemes_list = [], '', {}
+        print(f'{time.ctime()} - Reading power schemes...')
         for c in raw_list.stdout.decode('ascii')[74:]:
             if c != '\n' and c != '\r':
                 temp_string += c
@@ -64,10 +70,26 @@ class Arbiter():
         settings = self.read_settings()
         if self.__curr_scheme != self.__power_schemes[settings['active_scheme']]:
             subprocess.run("powercfg -s "+self.__power_schemes[settings['active_scheme']])
-            self.__curr_scheme = self.get_curr_scheme()
-        print(f'{time.ctime()} - Idle time is now being monitored. The current power scheme is \n{self.__curr_scheme}')
+            self.__curr_scheme = self.get_curr_scheme_id()
+        print(f'{time.ctime()} - Idle time and CPU usage is now being monitored. The current power scheme is {self.get_curr_scheme()}')
         while self.__running:
-            if self.get_idle_duration() > self.read_settings()['idle_threshold']:
+            settings = self.read_settings()
+            if (time.time()-self.__lastPoll) > settings['pollingSpeed']:
+                self.__cpu_usage = cpu_percent(percpu=True)
+                self.__lastPoll = time.time()
+                if not self.__afk:
+                    # for i in range(len(self.__cpu_usage)): # Single CPU usage
+                    #     if self.__cpu_usage[i] > int(settings['active_cpu_threshold']):
+                    #         self._set_to_high_performance()
+                    #         break
+                    #     elif i >= len(self.__cpu_usage)-1:
+                    #         self._set_to_default()
+                    if sum(self.__cpu_usage)/(len(self.__cpu_usage)+1) > int(settings['active_cpu_threshold']): # Average CPU usage
+                        self._set_to_high_performance()
+                    else:
+                        self._set_to_default()
+                    
+            if self.get_idle_duration() > settings['idle_threshold']:
                 if not self.__afk:
                     self._afk()
                     self.__afk = True
@@ -76,11 +98,20 @@ class Arbiter():
                     self._returned()
                     self.__afk = False
                 else:
-                    if self.__curr_scheme != self.get_curr_scheme():
-                        print(f'{time.ctime()} - Current scheme has been changed externally to \n{self.get_curr_scheme()}')
-                        self.__curr_scheme = self.get_curr_scheme()
+                    if self.__curr_scheme != self.get_curr_scheme_id():
+                        print(f'{time.ctime()} - Current scheme has been changed externally to {self.get_curr_scheme()}')
+                        self.__curr_scheme = self.get_curr_scheme_id()
+            self._draw_utilization_bar()
             time.sleep(1)
     # end _monitor
+
+    def _draw_utilization_bar(self):
+        bar_length = 20
+        fraction = round(sum(self.__cpu_usage)/(len(self.__cpu_usage)+1), 1)/100
+        arrow = "█" * int(fraction * bar_length)
+        padding = "-" * (bar_length - len(arrow))
+        print(f"\rCPU Usage: [{arrow}{padding}] {round(sum(self.__cpu_usage)/(len(self.__cpu_usage)+1), 1)}%\r", end="")
+    # end _draw_utilization_bar
 
     def get_idle_duration(self) -> float:
         return (win32api.GetTickCount() - win32api.GetLastInputInfo()) / 1000.0 # Convert to seconds from miliseconds
@@ -88,40 +119,61 @@ class Arbiter():
 
     def _afk(self) -> None:
         settings = self.read_settings()
-        if self.get_curr_scheme() != self.__power_schemes[settings['idle_scheme']]:
+        if self.get_curr_scheme_id() != self.__power_schemes[settings['idle_scheme']]:
             subprocess.run("powercfg -s "+self.__power_schemes[settings['idle_scheme']]) 
-            self.__curr_scheme = self.get_curr_scheme() #self.__power_schemes[settings['idle_scheme']]
-            print(f'{time.ctime()} - Computer has been idle for {self.get_idle_duration()} seconds and is now is now considered AFK, the Power Scheme has been set to \n{self.get_curr_scheme()}')
+            self.__curr_scheme = self.get_curr_scheme_id() #self.__power_schemes[settings['idle_scheme']]
+            print(f'{time.ctime()} - Computer has been idle for {self.get_idle_duration()} seconds and is now is now considered AFK, the Power Scheme has been set to {self.get_curr_scheme()}')
     # end _afk
 
     def _returned(self) -> None:
-        settings = self.read_settings() # Balanced or High Performance
-        if self.get_curr_scheme() != self.__power_schemes[settings['active_scheme']]:
+        settings = self.read_settings()
+        if self.get_curr_scheme_id() != self.__power_schemes[settings['active_scheme']]:
             subprocess.run("powercfg -s "+self.__power_schemes[settings['active_scheme']])
-            self.__curr_scheme = self.get_curr_scheme() #self.__power_schemes[settings['active_scheme']]
-            print(f'{time.ctime()} - Computer is no longer AFK, the Power Scheme has been set to \n{self.get_curr_scheme()}')
+            self.__curr_scheme = self.get_curr_scheme_id() #self.__power_schemes[settings['active_scheme']]
+            print(f'{time.ctime()} - Computer is no longer AFK, the Power Scheme has been set to {self.get_curr_scheme()}')
     # end _returned
 
-    def read_settings(self) -> json:
+    def _set_to_power_saver(self) -> None: # Not Used
+        if self.get_curr_scheme_id() != self.__power_schemes['Power saver']:
+            subprocess.run("powercfg -s "+self.__power_schemes['Power saver'])
+            self.__curr_scheme = self.get_curr_scheme_id() #self.__power_schemes['Power saver']
+            print(f'{time.ctime()} - Computer is no longer AFK, the Power Scheme has been set to {self.get_curr_scheme()}')
+    # end _setToPowerSaver
+
+    def _set_to_default(self) -> None:
+        settings = self.read_settings()
+        if self.get_curr_scheme_id() != self.__power_schemes[settings['active_scheme']]:
+            subprocess.run("powercfg -s "+self.__power_schemes[settings['active_scheme']])
+            self.__curr_scheme = self.get_curr_scheme_id() #self.__power_schemes[settings['active_scheme']]
+            print(f'{time.ctime()} - CPU utilization % is not over {self.read_settings()['active_cpu_threshold']}, the Power Scheme has been set to {self.get_curr_scheme()}')
+    # end _setToBalanced
+
+    def _set_to_high_performance(self) -> None:
+        if self.get_curr_scheme_id() != self.__power_schemes['High Performance']:
+            subprocess.run("powercfg -s "+self.__power_schemes['High Performance'])
+            self.__curr_scheme = self.get_curr_scheme_id() #self.__power_schemes['High Performance']
+            print(f'{time.ctime()} - CPU utilization % is over {self.read_settings()['active_cpu_threshold']}, the Power Scheme has been set to {self.get_curr_scheme()}')
+    # end _setToHighPerformance
+
+    def read_settings(self) -> dict:
         if Path('settings.json').exists():
             with open('settings.json', 'r') as file:
                 settings = json.load(file)
             return settings
         else:
-            self.write_settings(True)
+            self.create_settings()
+            return self.read_settings()
     # end read_settings
 
-    def write_settings(self, default: bool = False, settings: json =None) -> None:
-        if default == True:
-            settings = {
-                'idle_threshold': 180,
-                'idle_scheme': 'Power saver',
-                'active_scheme': 'High Performance',
-            }
-            with open('settings.json', 'w') as file:
-                json.dump(settings, file)
-        elif settings != None:
-            with open('settings.json', 'w') as file:
-                json.dump(settings, file)            
+    def create_settings(self) -> None:
+        settings = {
+            'pollingSpeed': 60,
+            'idle_threshold': 180,
+            'idle_scheme': 'Power saver',
+            'active_scheme': 'Balanced',
+            'active_cpu_threshold': 25.0,
+        }
+        with open('settings.json', 'w') as file:
+            json.dump(settings, file, indent=4)        
     # end write_settings
 # end Arbiter
